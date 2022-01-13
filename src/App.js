43184f8ee80ad13460c0e3c54b270b1490f9f3d3
Priyadarshi <priyadarshinayak2001@gmail.com>
2022-01-13 22:51:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import Expenses from './Components/Expenses/Expenses';
+import NewExpense from './Components/NewExpense/NewExpense';
+import React from 'react';
 
+const dummyList = [{
+  date: new Date(2021, 12, 1),
+  title: "Shampoo",
+  amount: 549
+},
+{
+  date: new Date(2021, 11, 14),
+  title: "Biscuit",
+  amount: 345
+}]
 function App() {
+  const [expenseList, setExpenseList] = React.useState(dummyList);
+
+  const addExpenseHandler = (item) => {
+    setExpenseList((prevState) => {
+      return [...prevState, item];
+    });
+  }
+
+  const deleteExpenseHandler = (toBeDeletedExpense) => {
+    setExpenseList((prevState) => {
+      let newExpensesList = []
+      for(let i = 0; i < prevState.length; i++){
+        if(isEqual(prevState[i], toBeDeletedExpense))continue;
+        newExpensesList.push(prevState[i]);
+      }
+      return newExpensesList;
+    });
+  }
+
+  const isEqual = (expenseItem1, expenseItem2) => {
+    return expenseItem1.title === expenseItem2.title && 
+           expenseItem1.amount === expenseItem2.amount && 
+           expenseItem1.date.toLocaleString() === expenseItem2.date.toLocaleString();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NewExpense onAddExpense = {addExpenseHandler}/>
+      <Expenses onDeleteExpense = {deleteExpenseHandler} expenseList = {expenseList}/>
     </div>
   );
 }
